c17f9d55e526dec57d92d60f88e4c7eb69ae7b99
Vaishnavi Mane <manevaishnavi635@gmail.com>
2026-02-18 19:55:04 +0530

    Update App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -7,27 +7,25 @@ import About from './About';
 import Contact from './Contact';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import App from './App.css'
-import {Switch,Route, Redirect} from "react-router-dom";
-
+import "./App.css"
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-    <Navbar />
-    <Switch>
-      <Route  exact path='/' Component={Home}/>    
-       <Route exact path='/about' Component={About}/>                            
-        <Route   exact path='/service' Component={Service}/>
-         <Route exact path='/contact' Component={Contact}/>
-         
-         <Redirect to="/"/>
-  
-   </Switch>
-   <Footer />
-    
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+
+      <Footer />
     </>
   )
 }
 
-export default App
+export default App;
