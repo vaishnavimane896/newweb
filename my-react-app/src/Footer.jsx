54c43eaa06034965098441d379b493f0cf70feb5
Vaishnavi Mane <manevaishnavi635@gmail.com>
2026-02-22 16:46:27 +0530

    Update Footer.jsx

--- my-react-app/src/Footer.jsx
+++ my-react-app/src/Footer.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <>
-    <footer className='bg-light text-center'>
-        <p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid, pariatur!
-        </p>
+    <footer className="footer-section">
+      <div className="container">
+        <div className="footer-content">
+          
+          <h3 className="footer-brand">Vaishnavi Mane</h3>
+          
+          <p className="footer-text">
+            Building modern web experiences with creativity & passion.
+          </p>
 
+          <div className="footer-links">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/service">Services</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+          </div>
+
+          <p className="footer-copy">
+            © {new Date().getFullYear()} All Rights Reserved.
+          </p>
+
+        </div>
+      </div>
     </footer>
-    
-    </>
   )
 }
 
