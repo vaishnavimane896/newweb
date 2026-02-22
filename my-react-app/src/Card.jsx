5d1ffd647292299c27ec97ff87784042cb7a4073
Vaishnavi Mane <manevaishnavi635@gmail.com>
2026-02-22 16:44:14 +0530

    Update Card.jsx

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -3,31 +3,22 @@ import web from "../src/images/japan.jpg";
 import { NavLink } from 'react-router-dom';
 
 const Card= (props) => {
-  return (
-    <>
-      <div className='col-10 mx-auto'>
-        <div className='row'>
-          <div className='col-md-4 col-10 mx-auto'>
-            <div className="card" >
-  <img src={props.imgsrc} className="card-img-top" alt={props.imgsrc} />
-  <div className="card-body">
-    <h5 className="card-title font-weight-bold">{props.title}</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <NavLink to=""  className="btn btn-primary">Go somewhere</NavLink>
-  </div>
-</div>
-</div>
-
-        
-          
-          
-        </div>
-
+ return (
+  <div className="col-md-4 col-10 mx-auto">
+    <div className="card">
+      <img src={props.imgsrc} className="card-img-top" alt="service" />
+      <div className="card-body">
+        <h5 className="card-title fw-bold">{props.title}</h5>
+        <p className="card-text">
+          We provide high quality development services with modern design.
+        </p>
+        <NavLink to="" className="btn btn-primary">
+          Explore
+        </NavLink>
       </div>
-
-    
-    </>
-  )
+    </div>
+  </div>
+);
 }
 
 export default Card
